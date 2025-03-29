81cddfaad5866fe223d256933a0830b4a4c81b95
vicky181294 <vigneshcv18@gmail.com>
2025-03-29 16:19:50 +0530

    modified cicd file#4

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "ecs_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.ecs_sg.id]
-  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  subnets            = [aws_subnet.public_subnet_1.id]
 }
 
 # ALB Target Groups for each service
